62d4e0e57ef489fd5a0c3ee3e69de321e431c37c
Imandevops <102887517+Imandevops@users.noreply.github.com>
2024-07-17 09:58:25 +0330

    Update test13.sh

--- test13.sh
+++ test13.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-read -p "enter your weight:" weight
-
-read -p "enter your height:" height
+read -p "Enter your weight (in kg): " weight
+read -p "Enter your height (in meters): " height
 
 bmi=$(bc <<< "scale=1; $weight / ($height * $height)")
 
+echo "Your BMI is $bmi"
 
-echo $bmi
+if (( $(echo "$bmi < 18.5" | bc -l) )); then
+    echo "You are underweight."
+elif (( $(echo "$bmi >= 18.5 && $bmi < 24.9" | bc -l) )); then
+    echo "You have a normal weight."
+elif (( $(echo "$bmi >= 25 && $bmi < 29.9" | bc -l) )); then
+    echo "You are overweight."
+else
+    echo "You are obese."
+fi
